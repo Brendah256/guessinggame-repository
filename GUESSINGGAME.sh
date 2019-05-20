4b591e740d6c7da3079e3ee3eb4568fff6d87d12
Brendah256 <brendahemolot@gmail.com>
2019-05-20 15:07:30 +0300

    all the 3 added files

--- GUESSINGGAME.sh
+++ GUESSINGGAME.sh
@@ -1,24 +1,47 @@
 #!/bin/bash
 
-nof=$(ls -l|wc -l)
-read -p "Guess the number of files in the current directory, type your guess and press enter:   " reply
+############################
 
-until [[ $reply -eq $nof ]];
+#Colour codes definition
+
+Color_Off='\033[0m'       # Text Reset
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BBlue='\033[1;34m'        # Blue
+BCyan='\033[1;36m'        # Cyan
+Green='\033[0;32m'        # Green
+
+#############################
+
+echo -e "${BCyan}\nGUESSING GAME written by BRENDAH RONAH ${Color_Off}"
+
+number_of_files=$(ls -l|wc -l)
+
+function your_guess {	
+       read -p "Guess the number of files in this directory, type your guess and press enter:   " reply
+       while [ -z $reply ] # This will tell whether the user did not enter a reply
+       do
+	   your_guess
+       done
+}
+
+your_guess
+
+while [ $reply -ne $number_of_files ]  # This is for whether the guess is not equal to the number of files in the directory
 do
-    if [[ $reply -gt $nof ]];then
-	echo " $reply is too high, please try again"
-else [[ $reply -lt $nof ]]
-	 echo " $reply is too low, please try again"
+    if [ $reply -gt $number_of_files ]
+    then
+	echo -e "${BBlue}\nSorry, your guess is too high, please try again.${Color_Off}"
+    else [ $reply -lt $number_of_files ]
+	 echo -e "${Green}\nSorry, your guess is too low, please try again.${Color_Off}"
     fi
-    read -p "Guess the number of files in the current directory, type your guess and press enter:   " reply
+    your_guess
 done
 
-echo "Congratulations, your guess is true"
-
-
-##Notes: 1.  'else' in the if conditional shouldn't have a semi colon at the end ( ; )
-##  2.S
-##  3.  when using the read -p command, you have to include the variable at the end of the description
-##  4.  Its good practice to always write your variables in lowercase as uppercase variables are usually
-##      interpreted as shell builtin variables
-##  5.  You have to close your if loop with fi
+if [ $reply -eq $number_of_files ]
+then
+    echo -e "${BGreen}\nCONGRATULATIONS!!!!!!, YOUR GUESS IS TRUE. ${Color_Off}"
+    exit
+else 
+    echo -e "${BRed}\nERROR!!....Please check your input and try again. ${Color_Off}"
+fi
---
+++ Makefile
@@ -0,0 +1,7 @@
+Create_README.md_file:
+
+	echo "# GUESSING GAME PROJECT" > README.md
+	echo `Date` >> README.md
+	echo `wc -l GUESSINGGAME.sh` >> README.md 
+
+
